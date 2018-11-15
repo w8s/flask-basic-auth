dfea58919a437ef4793c21f46d797456051c2b39
Todd Waits <twaits@sei.cmu.edu>
2018-11-15 17:38:09 -0500

    Basic Auth with Encrypted Passwords

--- auth.py
+++ auth.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, Response
 from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
+import bcrypt
 import os
 
 app = Flask(__name__)
@@ -17,6 +18,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True)
     password = db.Column(db.String(35))
+
+    @staticmethod
+    def hash_password(plain_text):
+        return bcrypt.hashpw(plain_text.encode('utf-8'), bcrypt.gensalt())
+
+    def check_password(self, plain_text):
+        return bcrypt.checkpw(plain_text.encode('utf-8'), self.password)
 
 
 ## Auth
@@ -47,7 +55,7 @@ def authenticate():
 def check_auth(username, password):
     """Checks that user creds are valid."""
     u = User.query.filter_by(username=username).first()
-    return username == u.username and password == u.password
+    return username == u.username and u.check_password(password)
 
 
 # Routes
@@ -70,8 +78,8 @@ def init_db():
     db.drop_all()
     db.create_all()
 
-    db.session.add(User(username="admin", password="secret"))
-    db.session.add(User(username="tow", password="secret"))
+    db.session.add(User(username="admin", password=User.hash_password("secret")))
+    db.session.add(User(username="tow", password=User.hash_password("secret")))
 
     db.session.commit()
 
